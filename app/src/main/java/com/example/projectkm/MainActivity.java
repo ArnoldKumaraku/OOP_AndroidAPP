fcefa15bd7932503500c8658f6e6a0e9b055207b
ArnoldKumaraku <62397739+ArnoldKumaraku@users.noreply.github.com>
2020-08-15 00:29:39 +0200

    aggiunta splashscreen potente

--- app/src/main/java/com/example/projectkm/MainActivity.java
+++ app/src/main/java/com/example/projectkm/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);//metterci qualcosa di carino
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
